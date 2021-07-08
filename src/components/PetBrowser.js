9c81c574446e1fb4e30c594a887828628ba0f276
Luis Martinez <luis_mmartinez@live.com>
2021-07-07 20:44:56 -0400

    commit tree b038b2033b28fb60a1c5f301958d945fb42a9f53

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,16 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  allPets = (pets) => {
+    return pets.map( petObj => {
+     return <Pet pet = {petObj} key = {petObj.id} onAdoptPet = {this.props.onAdoptPet} />
+    })
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards"> 
+      {this.allPets(this.props.pets)}
+      </div>
   }
 }
-
 export default PetBrowser
